1ccea757b3e6e0daa3f6868e246e47419cfd8d71
unknown <ethan.soobrayen@etu.unistra.fr>
2026-05-11 09:20:02 +0200

    Ouverture du fichier, pas de modifications en attendant la base de Guilaume

--- src/Form1.Designer.cs
+++ src/Form1.Designer.cs
@@ -32,9 +32,10 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
