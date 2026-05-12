83181619dc0e532fead0ededffb281f1d0a1cb0b
unknown <ethan.soobrayen@etu.unistra.fr>
2026-05-12 22:59:38 +0200

    Ajout de 2 UserControl : un pour les espèces en général, et un autre pour les espèces alliées

---
+++ src/UserControlPlanetes/UserControlEspecesAllies.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace UserControlPlanetes
+{
+    partial class UserControlEspecesAlliees
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblNomEspeceAlliee = new System.Windows.Forms.Label();
+            this.lblDegreBienveillance = new System.Windows.Forms.Label();
+            this.lblBienveillanceCommun = new System.Windows.Forms.Label();
+            this.lblOrigine = new System.Windows.Forms.Label();
+            this.lblPremierContactCommun = new System.Windows.Forms.Label();
+            this.lblDatePremierContact = new System.Windows.Forms.Label();
+            this.lblInstrument = new System.Windows.Forms.Label();
+            this.lblNbPlanete = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(61, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(240, 213);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblNomEspeceAlliee
+            // 
+            this.lblNomEspeceAlliee.AutoSize = true;
+            this.lblNomEspeceAlliee.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomEspeceAlliee.Location = new System.Drawing.Point(54, 248);
+            this.lblNomEspeceAlliee.Name = "lblNomEspeceAlliee";
+            this.lblNomEspeceAlliee.Size = new System.Drawing.Size(100, 37);
+            this.lblNomEspeceAlliee.TabIndex = 1;
+            this.lblNomEspeceAlliee.Text = "label1";
+            // 
+            // lblDegreBienveillance
+            // 
+            this.lblDegreBienveillance.AutoSize = true;
+            this.lblDegreBienveillance.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDegreBienveillance.Location = new System.Drawing.Point(189, 350);
+            this.lblDegreBienveillance.Name = "lblDegreBienveillance";
+            this.lblDegreBienveillance.Size = new System.Drawing.Size(58, 22);
+            this.lblDegreBienveillance.TabIndex = 2;
+            this.lblDegreBienveillance.Text = "label1";
+            // 
+            // lblBienveillanceCommun
+            // 
+            this.lblBienveillanceCommun.AutoSize = true;
+            this.lblBienveillanceCommun.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBienveillanceCommun.Location = new System.Drawing.Point(57, 350);
+            this.lblBienveillanceCommun.Name = "lblBienveillanceCommun";
+            this.lblBienveillanceCommun.Size = new System.Drawing.Size(126, 22);
+            this.lblBienveillanceCommun.TabIndex = 3;
+            this.lblBienveillanceCommun.Text = "Bienveillance :";
+            // 
+            // lblOrigine
+            // 
+            this.lblOrigine.AutoSize = true;
+            this.lblOrigine.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOrigine.Location = new System.Drawing.Point(57, 311);
+            this.lblOrigine.Name = "lblOrigine";
+            this.lblOrigine.Size = new System.Drawing.Size(58, 22);
+            this.lblOrigine.TabIndex = 4;
+            this.lblOrigine.Text = "label1";
+            // 
+            // lblPremierContactCommun
+            // 
+            this.lblPremierContactCommun.AutoSize = true;
+            this.lblPremierContactCommun.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPremierContactCommun.Location = new System.Drawing.Point(57, 388);
+            this.lblPremierContactCommun.Name = "lblPremierContactCommun";
+            this.lblPremierContactCommun.Size = new System.Drawing.Size(233, 22);
+            this.lblPremierContactCommun.TabIndex = 5;
+            this.lblPremierContactCommun.Text = "Premier contact effectué le :";
+            // 
+            // lblDatePremierContact
+            // 
+            this.lblDatePremierContact.AutoSize = true;
+            this.lblDatePremierContact.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDatePremierContact.Location = new System.Drawing.Point(57, 410);
+            this.lblDatePremierContact.Name = "lblDatePremierContact";
+            this.lblDatePremierContact.Size = new System.Drawing.Size(58, 22);
+            this.lblDatePremierContact.TabIndex = 6;
+            this.lblDatePremierContact.Text = "label1";
+            // 
+            // lblInstrument
+            // 
+            this.lblInstrument.AutoSize = true;
+            this.lblInstrument.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstrument.Location = new System.Drawing.Point(57, 458);
+            this.lblInstrument.Name = "lblInstrument";
+            this.lblInstrument.Size = new System.Drawing.Size(58, 22);
+            this.lblInstrument.TabIndex = 7;
+            this.lblInstrument.Text = "label1";
+            // 
+            // lblNbPlanete
+            // 
+            this.lblNbPlanete.AutoSize = true;
+            this.lblNbPlanete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNbPlanete.Location = new System.Drawing.Point(57, 289);
+            this.lblNbPlanete.Name = "lblNbPlanete";
+            this.lblNbPlanete.Size = new System.Drawing.Size(58, 22);
+            this.lblNbPlanete.TabIndex = 8;
+            this.lblNbPlanete.Text = "label1";
+            // 
+            // UserControlEspecesAlliees
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblNbPlanete);
+            this.Controls.Add(this.lblInstrument);
+            this.Controls.Add(this.lblDatePremierContact);
+            this.Controls.Add(this.lblPremierContactCommun);
+            this.Controls.Add(this.lblOrigine);
+            this.Controls.Add(this.lblBienveillanceCommun);
+            this.Controls.Add(this.lblDegreBienveillance);
+            this.Controls.Add(this.lblNomEspeceAlliee);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "UserControlEspecesAlliees";
+            this.Size = new System.Drawing.Size(372, 511);
+            this.Load += new System.EventHandler(this.UserControlEspecesAlliees_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblNomEspeceAlliee;
+        private System.Windows.Forms.Label lblDegreBienveillance;
+        private System.Windows.Forms.Label lblBienveillanceCommun;
+        private System.Windows.Forms.Label lblOrigine;
+        private System.Windows.Forms.Label lblPremierContactCommun;
+        private System.Windows.Forms.Label lblDatePremierContact;
+        private System.Windows.Forms.Label lblInstrument;
+        private System.Windows.Forms.Label lblNbPlanete;
+    }
+}
